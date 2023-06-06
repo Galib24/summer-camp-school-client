71fc1b35f71295efc32718766bbb9c030b3f3c0f
SQLAdmin <abuyeahia24@gmail.com>
2023-06-07 00:55:39 +0600

    set up router and tailwind and daysi Ui basic part

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+           <h2>this is home</h2> 
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#90f9d6",
+
+          "secondary": "#c6d6ff",
+
+          "accent": "#b994dd",
+
+          "neutral": "#212835",
+
+          "base-100": "#E6EAEF",
+
+          "info": "#528AE5",
+
+          "success": "#149443",
+
+          "warning": "#995B05",
+
+          "error": "#F62369",
+
+          "gray": "#A0A0A0",
+
+          "orange": "#ffa500"
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
