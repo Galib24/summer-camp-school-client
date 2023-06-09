ddd7ab3e7802ca0efca10d2a06213b980da6f0ed
SQLAdmin <abuyeahia24@gmail.com>
2023-06-09 17:19:56 +0600

    fixed google login user stop existing user added

--- src/Pages/SocialLogin/SocialLogin.jsx
+++ src/Pages/SocialLogin/SocialLogin.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { FaGoogle } from "react-icons/fa";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -6,7 +6,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
     const { googleSignIn } = useContext(AuthContext);
-    const [error, setError] = useState('');
+    // const [error, setError] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/'
@@ -17,8 +17,31 @@ const SocialLogin = () => {
             .then(res => {
                 const loggedUser = res.user;
                 console.log(loggedUser);
-                setError(loggedUser);
-                navigate(from, { replace: true });
+                // setError(loggedUser);
+                const saveStudent = { name: loggedUser.displayName, email: loggedUser.email }
+
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(saveStudent)
+                })
+                    .then(res => res.json())
+                    .then(() => {
+                        navigate(from, { replace: true });
+                        // if (data.insertedId) {
+                            
+                            // Swal.fire({
+                            //     position: 'top-end',
+                            //     icon: 'success',
+                            //     title: 'User created Successfully',
+                            //     showConfirmButton: false,
+                        //     //     timer: 1500
+                        //     // });
+                        // }
+                    })
+             
             })
     }
 
@@ -30,9 +53,9 @@ const SocialLogin = () => {
                 <button onClick={handleGoogleSignIn} className="btn btn-circle btn-outline">
                     <FaGoogle></FaGoogle>
                 </button>
-                {
+                {/* {
                     error ? <p className="text-red-500">something have problem with login</p> : ''
-                }
+                } */}
             </div>
         </div>
     );
