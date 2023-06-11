748345d5cab89e3c122c0ffd4475aa1678247390
SQLAdmin <abuyeahia24@gmail.com>
2023-06-11 22:31:25 +0600

    added payment form

--- src/Pages/DashboardSection/Payment/ChechkOutForm.jsx
+++ src/Pages/DashboardSection/Payment/ChechkOutForm.jsx
@@ -1,9 +1,11 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 
 const ChechkOutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState('')
 
 
     const handleSubmit = async (event) => {
@@ -17,32 +19,52 @@ const ChechkOutForm = () => {
         if (card === null) {
             return
         }
+        // console.log('card', card);
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            console.log('error', error);
+            setCardError(error.message);
+        }
+        else {
+            setCardError('');
+            console.log('payment method', paymentMethod);
+        }
 
     }
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            {cardError && <p className="text-red-500 ml-8">{cardError}</p>}
+
+        </>
     );
 };
 
