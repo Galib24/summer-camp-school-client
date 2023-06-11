01b4522d417975ab5e13384e21c9c89fd6428688
SQLAdmin <abuyeahia24@gmail.com>
2023-06-11 19:45:17 +0600

    basic install for payment system

--- src/Pages/DashboardSection/Payment/Payment.jsx
+++ src/Pages/DashboardSection/Payment/Payment.jsx
@@ -1,9 +1,22 @@
+import { loadStripe } from "@stripe/stripe-js";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import ChechkOutForm from "./ChechkOutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
-
+// TODO provide publish key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_gateway_PK);
 const Payment = () => {
     return (
-        <div>
-            <h2>payment</h2>
+        <div className="w-full text-center">
+            <SectionTitle
+                subHeading='to get best service make sure'
+                heading='Payment'
+            ></SectionTitle>
+
+            <Elements stripe={stripePromise}>
+                <ChechkOutForm></ChechkOutForm>
+            </Elements>
+
         </div>
     );
 };
